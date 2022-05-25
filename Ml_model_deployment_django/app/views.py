0cebcd1a66e9c8edbfd4498bb13c05edd100ad9b
Muruganandham R <muruganandham.r@ctlep.local>
2022-05-25 17:50:30 +0530

    commit

--- Ml_model_deployment_django/app/views.py
+++ Ml_model_deployment_django/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pandas as pd
+import seaborn as sns
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
